3aa531400f2dd9afc830a070a2c8489cf30d47e5
DJ-Fisher <1022411@student.sae.edu.au>
2021-06-01 18:14:04 +1000

    changed the material on the cube

--- Assets/Scripts/WeekOneChallenges.cs
+++ Assets/Scripts/WeekOneChallenges.cs
@@ -11,19 +11,20 @@ public class WeekOneChallenges : MonoBehaviour
     public int birthDay = 30;
     public int birthMonth = 01;
     public int birthYear = 2003;
-    public int height = 173;
-    //used to select the right words for the finished statement
+    public int HeightCm = 173;
+
+    //used to select the right words for the finished statement.
     public bool amWearingWatch = false;
     public int favNumber = 5;
     public int sumOfAgeAndNumber;
 
-    //used as placeholders to select the correct text when debuged
+    //used as placeholders to select the correct text when debuged out.
     public string watchClause;
     public string rumourClause;
 
     void Start()
-    {
-        //grabs the sum of the age and favorite number
+    {  
+        //grabs the sum of the age and favorite number, self explanatory.
         sumOfAgeAndNumber = age + favNumber;
 
         //simple if/else statement used to switch the words used in the final statement according to the amWearingWatch bool
@@ -38,9 +39,9 @@ public class WeekOneChallenges : MonoBehaviour
             rumourClause = "false";
         }
         
-        //the full statement debug (shortened because it's too long)
-        Debug.Log("Greetings, my name is " + myName + ", my age in years is " + age + ", that means my date of birth is " + birthDay + " " + birthMonth + 
-            " " + birthYear + ", I am " + height + "cm tall and in case you are wondering, the rumours are " + rumourClause + " I " + watchClause + 
+        //the full statement debug.
+        Debug.Log("Greetings, my name is " + myName + ", my age in years is " + age + ", and that means my date of birth is " + birthDay + " " + birthMonth + 
+            " " + birthYear + ", I am " + HeightCm + "cm tall and in case you are wondering, the rumours are " + rumourClause + " I " + watchClause + 
             " wearing a watch. Finally my favorite number is " + favNumber + " and if that is added to my age that will give me " + sumOfAgeAndNumber + ".");   
 
     }
